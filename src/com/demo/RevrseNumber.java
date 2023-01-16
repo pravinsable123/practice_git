213186a6ab378901a82905352195c11f3874b344
pravinsable <sabalepravin846@gmail.com>
2023-01-16 23:43:11 +0530

    first commit

---
+++ src/com/demo/RevrseNumber.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class RevrseNumber {
+public static void main(String[] args) {
+	int num=123;
+	int rev=0;
+	while(num!=0) {
+		int digit=num%10;
+		rev=rev*10+digit;
+		num=num/10;
+	}
+	System.out.println(rev);
+}
+}
